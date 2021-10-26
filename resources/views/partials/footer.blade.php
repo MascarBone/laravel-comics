9b758d8e9b8ac478eb8c1d38e70bfb273e590a94
MascarBone <simone.bonelli@gmail.com>
2021-10-26 17:56:29 +0200

    footer top

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,51 +1,55 @@
 <footer>
-    <div class="footer-wrap-top">
-        <div class="footer-text">
+    <div class="wrap-top">
+        <div class="footer-top">
+            <div class="footer-text">
 
-            <div class="footer-list">
+                <div class="footer-list footer-list-wrap">
 
-                <div class="footer-list">
-                    <h2>{{$links['title']}}</h2>
-                    <ul>
-                        @foreach ($links['list'] as $link)
-                            <li><a href="{{$link['route']}}">{{$link['name']}}</a></li>
-                        @endforeach
-                    </ul>
-                </div>
-        
-                <div class="footer-list">
-                    <h2>{{$shops['title']}}</h2>
-                    <ul>
-                        @foreach ($shops['list'] as $shop)
-                            <li><a href="{{$shop['route']}}">{{$shop['name']}}</a></li>
-                        @endforeach
-                    </ul>
-                </div>
-                
-                <div class="footer-list">
-                    <h2>{{$infos['title']}}</h2>
-                    <ul>
-                        @foreach ($infos['list'] as $info)
-                            <li><a href="{{$info['route']}}">{{$info['name']}}</a></li>
-                        @endforeach
-                    </ul>
-                </div>
-                
-                <div class="footer-list">
-                    <h2>{{$sites['title']}}</h2>
-                    <ul>
-                        @foreach ($sites['list'] as $site)
-                            <li><a href="{{$site['route']}}">{{$site['name']}}</a></li>
-                        @endforeach
-                    </ul>
-                </div>
+                    <div class="footer-list footer-list-content">
+                        <h2>{{$links['title']}}</h2>
+                        <ul>
+                            @foreach ($links['list'] as $link)
+                                <li><a href="{{$link['route']}}">{{$link['name']}}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
+            
+                    <div class="footer-list footer-list-content">
+                        <h2>{{$shops['title']}}</h2>
+                        <ul>
+                            @foreach ($shops['list'] as $shop)
+                                <li><a href="{{$shop['route']}}">{{$shop['name']}}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
                     
+                    <div class="footer-list footer-list-content">
+                        <h2>{{$infos['title']}}</h2>
+                        <ul>
+                            @foreach ($infos['list'] as $info)
+                                <li><a href="{{$info['route']}}">{{$info['name']}}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    
+                    <div class="footer-list footer-list-content">
+                        <h2>{{$sites['title']}}</h2>
+                        <ul>
+                            @foreach ($sites['list'] as $site)
+                                <li><a href="{{$site['route']}}">{{$site['name']}}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
+                        
+                </div>
+
+                <p>All site Content TM and &Coproduct; 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved. <a href="#">Cookies Settings</a></p>
+                        
             </div>
-                    
-        </div>
 
-        <div class="footer-logo">
-            <img src="{{asset('images/dc-logo-bg.png')}}" alt="">
+            <div class="footer-logo">
+                <img src="{{asset('images/dc-logo-bg.png')}}" alt="">
+            </div>
         </div>
     </div>
     <div class="footer-wrap-bot">
