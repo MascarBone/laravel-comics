ae57b0b345bab5529f773e8a953e791cc61910b8
MascarBone <simone.bonelli@gmail.com>
2021-10-29 17:16:08 +0200

    font Lato added

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,13 @@
 <header>
     <div class="wrap-top">
         <div class="container">
+            <div>
+                <a href="#">dc power&#8480; visa &reg;</a>
+            </div>
 
+            <div>
+                <a href="#">additional dc sites &triangledown;</a>
+            </div>
         </div>
     </div>
     <div class="wrap-bot">
@@ -16,6 +22,15 @@
                     <li><a href="{{$link['route'] ? route($link['route']) : route('home')}}">{{$link['name']}}</a></li>
                 @endforeach
             </ul>
+
+            <div>
+                <a href="#">shop &triangledown;</a>
+            </div>
+
+            <div class="searchbar">
+                <input type="text" placeholder="Search">
+                <i class="fas fa-search"></i>
+            </div>
         </div>
     </div>
     
