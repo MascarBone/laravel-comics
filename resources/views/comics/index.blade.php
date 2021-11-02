1dcb1010c00b3c6ad6c61ae2c23e6c2c3599eff5
MascarBone <simone.bonelli@gmail.com>
2021-11-02 13:29:41 +0100

    comics index, buy comics bar added

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="cards">
+    <div class="container cards">
         @foreach ($cards as $card)
         <div class="card-wrap">
             <a href="{{route('comics.show', ['num'=>$loop->index])}}">
@@ -16,5 +16,31 @@
             </a>
         </div>
         @endforeach    
+    </div>
+
+    <div class="wrapper-bg-blue">
+        <div class="container box">
+                <div class="box-item">
+                    <img src="/images/buy-comics-digital-comics.png" alt="digital comics image">
+                    <p class="upper-case">digital comics</p>
+                </div>
+                <div class="box-item">
+                    <img src="/images/buy-comics-merchandise.png" alt="digital comics image">
+                    <p class="upper-case">dc merchandise</p>
+                </div>
+                <div class="box-item">
+                    <img src="/images/buy-comics-subscriptions.png" alt="digital comics image">
+                    <p class="upper-case">subscritions</p>
+                </div>
+                <div class="box-item">
+                    <img src="/images/buy-comics-shop-locator.png" alt="digital comics image">
+                    <p class="upper-case">comic shop locator</p>
+                </div>
+                <div class="box-item">
+                    <img src="/images/buy-dc-power-visa.svg" alt="digital comics image">
+                    <p class="upper-case">dc power visa</p>
+                </div>
+
+        </div>
     </div>
 @endsection 
